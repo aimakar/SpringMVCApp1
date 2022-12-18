5f6382374ae7f4833f3c20732965b7e4b818974e
Andrey <aimakar@mail.ru>
2022-12-19 01:02:11 +0300

    Lesson 16

--- src/main/java/ru/makarov/springcourse/HelloController.java
+++ src/main/java/ru/makarov/springcourse/HelloController.java
@@ -1,2 +1,12 @@
-package ru.makarov.springcourse;public class HelloController {
+package ru.makarov.springcourse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello-world")
+    public String sayHello() {
+        return "hello_world";
+    }
 }
