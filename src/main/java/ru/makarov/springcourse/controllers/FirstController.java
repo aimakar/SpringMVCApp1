2a450ba988e55d53f8d2bd4bf9d0d48303ec6f51
Andrey <aimakar@mail.ru>
2022-12-20 01:06:29 +0300

    Lesson 20

--- src/main/java/ru/makarov/springcourse/controllers/FirstController.java
+++ src/main/java/ru/makarov/springcourse/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package ru.makarov.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,14 +13,45 @@ import javax.servlet.http.HttpServletRequest;
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname) {
-        System.out.println("Hello, " + name + " " + surname);
+    public String helloPage(@RequestParam(value = "a", required = false) int a,
+                            @RequestParam(value = "b", required = false) int b,
+                            @RequestParam(value = "operation", required = false) String operation,
+                            Model model) {
+        model.addAttribute("message", "Result of " + String.valueOf(a) + " " + operation+ " " + String.valueOf(b) + " is ");
+        //?a=2&b=7&operation=multiplication
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
     public String goodByePage() {
         return "first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculator (@RequestParam(value = "a", required = false) int a,
+                              @RequestParam(value = "b", required = false) int b,
+                              @RequestParam(value = "operation", required = false) String operation,
+                              Model model) {
+            double result;
+            switch (operation) {
+                case ("multiplication"):
+                    result = a * b;
+                    break;
+                case ("addition"):
+                    result = a + b;
+                    break;
+                case ("substraction"):
+                    result = a - b;
+                    break;
+                case ("division"):
+                    result = a / (double) b;
+                    break;
+                default:
+                    result = 0;
+                    break;
+            }
+            model.addAttribute("result", "Result of " + String.valueOf(a) + " " + operation + " " + String.valueOf(b) + " is " + String.valueOf(result));
+            //?a=2&b=7&operation=multiplication
+            return "first/calculator";
     }
 }
